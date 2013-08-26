3d0367f2c1443019d4abecc7a7403dbcb92dfb34
Jozef Reisinger <jozef.reisinger@gmail.com>
2013-08-26 12:02:15 +0000

    processes listing script

---
+++ ps.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+# List running processes by manually inspecting /proc.
+# Shoud be run as root/sudo.
+use strict;
+use warnings;
+
+opendir my $PROC, '/proc' or die "$!";
+while ( readdir $PROC ) {
+    next unless /^\d+$/;    # skip non-process dirs
+    my $pid     = $_;
+    my $cmdline = `cat /proc/$_/cmdline`;
+    my $user    = getpwuid( ( lstat "/proc/$pid" )[4] );
+    printf "%s %s %s\n", $pid, $user, $cmdline;
+}
+close $PROC;
