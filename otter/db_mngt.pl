2412e224fdd3c2f0fe3e18c2d54de9ca8850f677
Jozef Reisinger <jreisinger@users.noreply.github.com>
2013-09-24 15:01:42 +0200

    Create db_mngt.pl

---
+++ otter/db_mngt.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+# Database management example using SQLite
+use strict;
+use warnings;
+use DBI;
+
+my $dbfile = '/tmp/test.db';
+
+# Connect to DB and receive connection handle
+my $dbh = DBI->connect( "DBI:SQLite:dbname=$dbfile", "", "" );
+$dbh->{RaiseError} = 1;    # do this, or check every call for errors
+
+# Populate the database
+$dbh->do(q{CREATE TABLE hosts (name varchar(15), ipaddr varchar(15))});
+$dbh->do(q{INSERT INTO hosts VALUES ('albert', '192.168.1.10')});
+$dbh->do(q{INSERT INTO hosts VALUES ('blade', '192.168.1.11')});
+$dbh->do(q{INSERT INTO hosts VALUES ('notebook', '192.168.1.51')});
+
+# Retrieve SELECT results
+my $all = $dbh->selectall_arrayref(q{SELECT * FROM hosts});
+foreach my $row (@$all) {
+    my ( $name, $ipaddr ) = @$row;
+    printf "%-15s %15s\n", $name, $ipaddr;
+}
+
+# Close the connection
+$dbh->disconnect;
+
+unlink $dbfile;
